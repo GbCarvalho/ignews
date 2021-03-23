4b0f970280f6e50346ff5f2fcfa71611462b83a7
Gabriel Carvalho <braga.gabrielcarvalho@gmail.com>
2021-03-23 15:22:40 -0300

    Components and Home page style done
    Done the Header, Home page, Sign In Button and Subscribe button styles

---
+++ src/components/SignInButton/index.tsx
@@ -0,0 +1,21 @@
+import { FaGithub } from "react-icons/fa";
+import { FiX } from "react-icons/fi";
+
+import styles from "./styles.module.scss";
+
+export function SignInButton() {
+  const isUserLoggedIn = true;
+
+  return isUserLoggedIn ? (
+    <button type="button" className={styles.signInButton}>
+      <FaGithub color="#04b361" />
+      Gabriel Carvalho
+      <FiX className={styles.closeIcon} />
+    </button>
+  ) : (
+    <button type="button" className={styles.signInButton}>
+      <FaGithub color="#eba417" />
+      Sign in with GitHub
+    </button>
+  );
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,12 @@
 import { AppProps } from "next/app";
-import Head from "next/head";
+import { Header } from "../components/Header";
 
 import "../styles/global.scss";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Head>
-        <title>ig.news</title>
-      </Head>
+      <Header />
       <Component {...pageProps} />
     </>
   );
