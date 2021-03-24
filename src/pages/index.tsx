63ca88c7fab39e28cdc3ac6a46e5584731c1ccc4
Gabriel Carvalho <braga.gabrielcarvalho@gmail.com>
2021-03-24 11:43:51 -0300

    Changed from SSR to SSG
    Changed a function that doesn't need server side rendenring for a Static Site Generation so it consumes less from the stripe API

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from "next";
+import { GetStaticProps } from "next";
 import Head from "next/head";
 import { SubscribeButton } from "../components/SubscribeButton";
 import { stripe } from "../services/stripe";
@@ -40,7 +40,7 @@ export default function Home({ product }: HomeProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve("price_1IYEbBH2LZ8aEjfAHGH7w9Bu");
 
   const product = {
@@ -55,5 +55,6 @@ export const getServerSideProps: GetServerSideProps = async () => {
     props: {
       product,
     },
+    revalidate: 60 * 60 * 24, // 24 hours
   };
 };
