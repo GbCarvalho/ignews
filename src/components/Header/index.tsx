d2596bf08918207de95bbf33edb9b66cb775ec00
Gabriel Carvalho <braga.gabrielcarvalho@gmail.com>
2021-03-31 15:21:38 -0300

    Starting with Prismic CMS
    Made the sync with Prismic CMS API and designed and implemented the Posts page where you can see a preview of the news

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,6 @@
 import { SignInButton } from "../SignInButton";
 import styles from "./styles.module.scss";
+import { ActiveLink } from "../ActiveLink";
 
 export function Header() {
   return (
@@ -7,10 +8,12 @@ export function Header() {
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="ig.news" />
         <nav>
-          <a href="" className={styles.active}>
-            Home
-          </a>
-          <a href="">Posts</a>
+          <ActiveLink activeClassName={styles.active} href="/">
+            <a>Home</a>
+          </ActiveLink>
+          <ActiveLink activeClassName={styles.active} href="/posts">
+            <a>Posts</a>
+          </ActiveLink>
         </nav>
 
         <SignInButton />
