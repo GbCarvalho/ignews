356c8b583d1e601606885113799d4bbc0e47b6f8
Gabriel Carvalho <braga.gabrielcarvalho@gmail.com>
2021-09-27 15:34:43 -0300

    SignInButton testing if user is logged trough mocked function from ts-jest

--- src/components/SignInButton/SignInButton.spec.tsx
+++ src/components/SignInButton/SignInButton.spec.tsx
@@ -9,10 +9,29 @@ describe("SignInButton Component", () => {
   it("renders correctly when user is not authenticated", () => {
     const useSessionMocked = mocked(useSession);
 
-    useSessionMocked.mockReturnValue([null, false]);
+    useSessionMocked.mockReturnValueOnce([null, false]);
 
     render(<SignInButton />);
 
     expect(screen.getByText("Sign in with GitHub")).toBeInTheDocument();
+  });
+
+  it("renders correctly when user is authenticated", () => {
+    const useSessionMocked = mocked(useSession);
+
+    useSessionMocked.mockReturnValueOnce([
+      {
+        user: {
+          name: "John Doe",
+          email: "john.doe@example.com",
+        },
+        expires: "example-expires",
+      },
+      true,
+    ]);
+
+    render(<SignInButton />);
+
+    expect(screen.getByText("John Doe")).toBeInTheDocument();
   });
 });
