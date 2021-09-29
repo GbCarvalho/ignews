17f7675445319b922d429bb45ca4febd8e4f4d4b
Gabriel Carvalho <braga.gabrielcarvalho@gmail.com>
2021-09-29 18:24:06 -0300

    Home page component parameter fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -31,7 +31,7 @@ export default function Home({ product }: HomeProps) {
             <span>for {product.amount} month</span>
           </p>
 
-          <SubscribeButton priceId={product.priceId} />
+          <SubscribeButton />
         </section>
 
         <img src="/images/avatar.svg" alt="Girl coding" />
